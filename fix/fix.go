2bd05c9a9dbbc1768386b64312a3dea19eff1ae3
Nate Finch <nate.finch@gmail.com>
2017-11-02 14:35:51 -0400

    can fix paths now

--- fix/fix.go
+++ fix/fix.go
@@ -8,14 +8,29 @@ import (
 )
 
 // Fix replaces orig with repl in tpl.  tpl must be a valid go template.  Orig
-// must be a valid template  FieldNode or IdenitifierNode (i.e. a function or a
-// .Foo.Bar.Baz).
+// must be a valid template function name or . path (e.g. .Foo.Bar).  Paths *must* start with a ".".
 func Fix(tpl string, orig, repl string) (string, error) {
 	tree, err := parse.ParseNoFuncs("", tpl, "", "")
 	if err != nil {
 		return "", err
 	}
-	s := &state{orig, repl}
+	s := &state{}
+	if strings.HasPrefix(orig, ".") {
+		// append a dot at the end to ensure we get full word matching
+		if strings.HasSuffix(orig, ".") {
+			s.path = orig
+		} else {
+			s.path = orig + "."
+		}
+		if strings.HasSuffix(repl, ".") {
+			s.repl = repl
+		} else {
+			s.repl = repl + "."
+		}
+	} else {
+		s.fn = orig
+		s.repl = repl
+	}
 	for k, v := range tree {
 		if k != "" {
 			fmt.Printf("parsing tree %q\n", k)
@@ -30,7 +45,9 @@ func Fix(tpl string, orig, repl string) (string, error) {
 }
 
 type state struct {
-	orig, repl string
+	fn   string
+	path string
+	repl string
 }
 
 // walk steps through the major pieces of the template structure.
@@ -46,8 +63,11 @@ func (s *state) walk(node parse.Node) {
 	case *parse.IfNode:
 		s.walkBranch(node.BranchNode)
 	case *parse.RangeNode:
+		s.walk(node.Pipe)
 		s.walkBranch(node.BranchNode)
 	case *parse.WithNode:
+		s.walk(node.List)
+		s.walk(node.Pipe)
 		s.walkBranch(node.BranchNode)
 	case *parse.ListNode:
 		for _, n := range node.Nodes {
@@ -58,7 +78,7 @@ func (s *state) walk(node parse.Node) {
 	case *parse.StringNode, *parse.TextNode:
 		// nothing to do
 	case *parse.IdentifierNode:
-		if node.Ident == s.orig {
+		if node.Ident == s.fn {
 			node.Ident = s.repl
 		}
 	case *parse.CommandNode:
@@ -66,7 +86,13 @@ func (s *state) walk(node parse.Node) {
 			s.walk(n)
 		}
 	case *parse.FieldNode:
-		// ignore for now
+		// append a dot at the end to ensure we get full word matching
+		ident := "." + strings.Join(node.Ident, ".") + "."
+		if !strings.Contains(ident, s.path) {
+			return
+		}
+		val := strings.Trim(strings.Replace(ident, s.path, s.repl, 1), ".")
+		node.Ident = strings.Split(val, ".")
 	default:
 		panic(fmt.Sprintf("unknown node: %T", node))
 	}
--- fix/fix_test.go
+++ fix/fix_test.go
@@ -2,13 +2,25 @@ package fix
 
 import "testing"
 
-func TestFix(t *testing.T) {
-	tpl := `{{index "foobar" "b"}}`
+func TestFixFunc(t *testing.T) {
+	tpl := `{{index "index" "d"}}`
 	out, err := Fix(tpl, "index", "strings.Index")
 	if err != nil {
 		t.Fatal(err)
 	}
-	expected := `{{strings.Index "foobar" "b"}}`
+	expected := `{{strings.Index "index" "d"}}`
+	if out != expected {
+		t.Fatalf("expected:\n%q\n\nbut got:\n%q", expected, out)
+	}
+}
+
+func TestFixPath(t *testing.T) {
+	tpl := `{{.Foo.Bar ".Foo.Bar"}}`
+	out, err := Fix(tpl, ".Foo.Bar", ".Foo.Baz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	expected := `{{.Foo.Baz ".Foo.Bar"}}`
 	if out != expected {
 		t.Fatalf("expected:\n%q\n\nbut got:\n%q", expected, out)
 	}
